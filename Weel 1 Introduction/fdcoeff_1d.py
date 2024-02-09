525cd5b0525ea560ffb062c3f7eb0ab848ca67df
jacoboep <s204408@student.dtu.dk>
2024-02-09 17:25:28 +0100

    lort

--- Weel 1 Introduction/fdcoeff_1d.py
+++ Weel 1 Introduction/fdcoeff_1d.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from typing import List
 
@@ -78,14 +79,87 @@ def fdcoeff_1d_general(x: List[float], x0: float):
     M = np.zeros((r, r))
     for i in range(r):
         for j in range(r):
-            M[i, j] = (x[i] - x0)**j / np.math.factorial(j)
+            M[i, j] = (x[i] - x0)**j / math.factorial(j)
 
     # Inversing M
     A = np.linalg.inv(M)
     
     # Scaling
-    A = A / dx0**np.arange(r)
+    A = A / (dx0**np.arange(r))
     return A
+
+def eval_fdcoeff_1d_general(fun: callable, x: List[float], a: int, b: int):
+    """
+    Computes coefficients of one-dimensional finite difference schemes on an even stencil.
+    """
+    u = fun(x)
+    ux = np.zeros(len(x))
+    for i in range(0, len(x)):
+        a_i = max(0, i - a)
+        b_i = min(len(x), i + b)
+        x_i = x[a_i:b_i+1] # Stencil
+        u_i = u[a_i:b_i+1]
+        A = fdcoeff_1d_general(x_i, x[i])
+        ux[i] = np.dot(A[1, :], u_i)
+    return ux
+
+def convergence_test_general():
+    N = [15, 45, 135, 405]
+    C = [0.0000000001, 0.5, 1.5, 2.5]
+
+    fun = lambda x: 1.0/(1.0 + 25.0*x**2)
+    diff_fun = lambda x: -50.0*x/(1.0 + 25.0*x**2)**2
+
+    a = 2
+    b = 2
+
+    error = np.zeros((len(C), len(N)))
+    roc = np.zeros(len(C))
+    TL = np.zeros((len(C), len(N)))
+
+    for i, c in enumerate(C):
+        for j, n in enumerate(N):
+            nn = int((n-1)/2 + 1)
+            ds = 1/(nn-1)
+            s = np.transpose(np.arange(0, nn))*ds
+            xi = np.tanh(c * s)/np.tanh(c)
+            x = np.zeros(2*nn-1)
+            x[0 : nn] = -1 + xi
+            x[nn-1 ::] = 1 - xi[nn-1::-1]
+            u = fun(x)
+            ux = diff_fun(x)
+            ux_approx = eval_fdcoeff_1d_general(fun, x, a, b)
+
+            x_test = np.linspace(-10, 10, 1000)
+
+            plt.figure()
+            #plt.plot(x, ux, label="Exact")
+            plt.plot(x, ux_approx, label="Approx")
+            plt.legend()
+
+            plt.figure()
+            plt.scatter(x, u)
+            plt.plot(x_test, fun(x_test))
+            plt.show()
+
+            error[i, j] = np.abs(ux_approx - ux).max()
+
+        _roc = np.polyfit(np.log(N), np.log(error[i, :]),1)
+        roc[i] = _roc[0]
+        TL[i, :] = np.polyval(_roc, np.log(N))
+    
+    # plot 2 subplots
+    plt.figure()
+    plt.suptitle(f"Convergence test with a = {a}, b = {b}")
+    for i, c in enumerate(C):
+        plt.loglog(N, error[i, :], label=f"C = {c}. Slope: {roc[i]:.2f}", marker="o")
+        plt.semilogx(N, TL[i, :])
+        plt.xlabel("N")
+        plt.ylabel("Error")
+        plt.legend()
+    plt.show()
+        
+    stop = True
 
 
 if __name__ == "__main__":
@@ -95,7 +169,5 @@ if __name__ == "__main__":
         convergence_test_uniform()
 
     if True:
-        x = np.array([0, 1, 2])
-        x0 = 1
-        A = fdcoeff_1d_general(x, x0)
+        convergence_test_general()
 
