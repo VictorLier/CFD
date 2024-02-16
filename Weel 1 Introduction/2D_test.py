be99a8de268293573ecc13a0f2417a68919f06db
VictorLier <victor@lier.dk>
2024-02-16 08:45:18 +0100

    Rode og balade

--- Weel 1 Introduction/2D_test.py
+++ Weel 1 Introduction/2D_test.py
@@ -15,21 +15,22 @@ DERy = 1
 
 #Initialize
 k = 2*math.pi/L
-#fun = math.sin(k*X)*math.sin(k*Y)
-#dfun = k*k*math.cos(k*X)*math.cos(k*Y)
+fun = lambda x,y: np.sin(k*x)*np.sin(k*y)
+print(fun(0.8,0.8))
+dfun = lambda x,y: k*k*np.cos(k*X)*np.cos(k*Y)
 D = L/(N-1)
 error = np.zeros((1, len(N)))
 
 for i, N in enumerate(N):
     x = np.linspace(0, L, N)
     y = np.linspace(0, L, N)
-
     X, Y = np.meshgrid(x,y)
 
-    fun = np.sin(k*X)*np.sin(k*Y)
+    f = fun(X,Y)
 
-    Dx = diffmatrix_1d_general(DERx,X[0],a,b) # Lidt Wired med X[0]
-    Dy = diffmatrix_1d_general(DERy,Y[0],a,b)
+    Dx = diffmatrix_1d_general(DERx,X[0,:],a,b) # Lidt Wired med X[0,:]
+    Dy = diffmatrix_1d_general(DERy,Y[:,0],a,b)
+    print(Dx)
 
     IdentMat = np.eye(N)
 
@@ -38,10 +39,14 @@ for i, N in enumerate(N):
 
     print(DX)
 
+
+    Fx = 1/(2*D)* np.dot(Dx,f)
+
+
     D_comb = np.hstack((DX,DY)) # ChatGPT bedste bud
 
     fun_flat = fun.flatten()
-    dfun_flat = np.dot(D_comb,fun_flat)
+    dfun_flat = 1/(2*D) * np.dot(D_comb,fun_flat)
 
     dfun_flat_2D = dfun_flat.reshape(fun.shape)
 
