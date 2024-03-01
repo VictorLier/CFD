14afc387860e450023925dd8874b7ca1614f4661
VictorLier <victor@lier.dk>
2024-03-01 15:57:14 +0100

    Det virker

--- Modul 2/SparseTest.py
+++ Modul 2/SparseTest.py
@@ -4,17 +4,11 @@ import scipy.sparse as sps
 n = 9
 
 AP = np.arange(1,n+1)
-AS = np.arange(1,n+1)
-AW = np.arange(1,n+1)
-AN = np.arange(1,n+1)
-AE = np.arange(1,n+1)
+AS = np.arange(10,n+10)
+AW = np.arange(20,n+20)
+AN = np.arange(30,n+30)
+AE = np.arange(40,n+40)
 
-D = sps.diags(AP,-1)
-
+D = sps.spdiags([AP,AE,AN,AS,AW],[0,-3,-1,1,3],n,n).T
 
 print(D.toarray())
-
-
-
-
-
