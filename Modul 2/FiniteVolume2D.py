baf2956a7e39893dcbcf2b0556224e83821b4bec
VictorLier <victor@lier.dk>
2024-03-08 19:14:48 +0100

    Lidt fikseri

--- Modul 2/FiniteVolume2D.py
+++ Modul 2/FiniteVolume2D.py
@@ -27,6 +27,9 @@ def convectiveVelocityField(problem, n, xf):
         return UFw, UFe, VFs,VFn
     
 def convective_face_matrix(n, dx, Pe, UFw, UFe, VFs, VFn):
+    ''''
+    This function calculates the convective face matrix with
+    '''
     Fe = Pe * UFe * dx
     Fw = Pe * UFw * dx
     Fs = Pe * VFs * dx
@@ -112,7 +115,7 @@ def impose_boundary(n, dx, xc, problem, aW, aE, aS, aN, aP):
         aE[:,-1] = 0
         ## South
         aP[0,:] = aP[0,:] - aS[0,:]
-        s[0,:] = s[0,:] - 2*Tgs*aS[:,-1]
+        s[0,:] = s[0,:] - 2*Tgs*aS[0,:]
         aS[0,:] = 0
         
         return s, aW, aE, aS, aN, aP
@@ -136,16 +139,62 @@ def solve(A, s):
 
     return T.reshape(n,n,order='F'), solve_time
 
-def extrapolate_temperature_field_to_walls(n, dx, fvscheme, problem, T):
-    TT = np.zeros((n+2,n+2))
-    return TT
+def extrapolate_temperature_field_to_walls(n, dx, fvscheme, problem, T, xc):
+    if problem == 1:
+        if fvscheme == 'cds':    
+            TT = np.zeros((n+2,n+2))
+            TT[1:-1,1:-1] = T
+            TT[:,0] = 0
+            TT[:,-1] = 1
+            TT[0,:] = TT[1,:]
+            TT[-1,:] = TT[-2,:]
+            return TT
+    
+        if fvscheme == 'uds':
+            TT = np.zeros((n+2,n+2))
+            TT[1:-1,1:-1] = T
+            TT[:,0] = 2*0 - TT[:,1]
+            TT[:,-1] = TT[:,-2]
+            TT[0,:] = TT[1,:]
+            TT[-1,:] = TT[-2,:]
+            return TT
+        
+    if problem == 2:
+        if fvscheme == 'cds':
+            _xc = np.zeros(len(xc)+2)
+            _xc[1:-1] = xc
+            _xc[0] = xc[0] - dx/2
+            _xc[-1] = xc[-1] + dx/2
+            TT = np.zeros((n+2,n+2))
+            TT[1:-1,1:-1] = T
+            TT[:,0] = 0
+            TT[:,-1] = 1
+            TT[0,:] = _xc
+            TT[-1,:] = TT[-2,:]
+            return TT
 
+        if fvscheme == 'uds':
+            _xc = np.zeros(len(xc)+2)
+            _xc[1:-1] = xc
+            _xc[0] = xc[0] - dx/2
+            _xc[-1] = xc[-1] + dx/2
+            TT = np.zeros((n+2,n+2))
+            TT[1:-1,1:-1] = T
+            TT[:,0] = TT[:,1]
+            TT[:,-1] = 2*1 - TT[:,-2]
+            TT[0,:] = 2*_xc - TT[1,:]
+            TT[-1,:] = TT[-2,:]
+            return TT
     stop = True
 
-def plot_temperature_field(xc, T):
+def plot_temperature_field(xc, TT, dx, L):
+    _xc = np.zeros(len(xc)+2)
+    _xc[1:-1] = xc
+    _xc[0] = xc[0] - dx/2
+    _xc[-1] = xc[-1] + dx/2
     plt.figure()
     # Make a grid plot with temperature as the color usin pcolormesh
-    plt.pcolormesh(xc, xc, T)
+    plt.pcolormesh(_xc, _xc, TT)
     # Add label to colorbar
     plt.colorbar(label='Temperature')
     plt.xlabel('x-axis')
@@ -154,7 +203,7 @@ def plot_temperature_field(xc, T):
     plt.title('Temperature distribution')
 
     plt.figure()
-    plt.contourf(xc, xc, T)
+    plt.contourf(_xc, _xc, TT)
     plt.colorbar(label='Temperature')
     plt.xlabel('x-axis')
     plt.ylabel('y-axis')
@@ -164,8 +213,8 @@ def plot_temperature_field(xc, T):
     # Plot the surface 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    X, Y = np.meshgrid(xc, xc)
-    ax.plot_surface(X, Y, T, cmap='viridis')
+    X, Y = np.meshgrid(_xc, _xc)
+    ax.plot_surface(X, Y, TT, cmap='viridis')
     ax.set_xlabel('x-axis')
     ax.set_ylabel('y-axis')
     ax.set_zlabel('Temperature')
@@ -192,21 +241,21 @@ def do_simulation(n, L, Pe, problem, fvscheme, plot = True):
     s, aW, aE, aS, aN, aP = impose_boundary(n, dx, xc, problem, aW, aE, aS, aN, aP)
     D = assemble_matrix(n, aW, aE, aS, aN, aP)
     T, solve_time = solve(D,s)
-    TT = extrapolate_temperature_field_to_walls(n, dx, fvscheme, problem, T)
+    TT = extrapolate_temperature_field_to_walls(n, dx, fvscheme, problem, T, xc)
     if plot:
-        plot_temperature_field(xc, T)
-    return T, solve_time
+        plot_temperature_field(xc, TT, dx, L)
+    return T, TT, solve_time
 
 if __name__=="__main__":
-    n = 250
+    n = 50
     L = 1
     # P = 1.5
     # Pe = P * n
     Pe = 10
-    problem = 1
-    fvscheme = 'cds'
+    problem = 2
+    fvscheme = 'uds'
 
-    T, solve_time = do_simulation(n, L, Pe, problem, fvscheme, plot = True)
+    T, TT, solve_time = do_simulation(n, L, Pe, problem, fvscheme, plot = True)
 
 
     plt.show()
