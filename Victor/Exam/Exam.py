d6621a86636488300b396efd95db7923df69c412
VictorLier <victor@lier.dk>
2024-05-20 15:43:26 +0200

    Exam py klargjort

--- Victor/Exam/Exam.py
+++ Victor/Exam/Exam.py
@@ -5,3 +5,34 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
+    if True: # 01
+        print("stop")
+    
+
+    if False: # 02
+        print("stop")
+
+
+    if False: # 03
+        print("stop")
+    
+
+    if False: # 04
+        print("stop")
+    
+
+    if False: # 05
+        print("stop")
+    
+
+    if False: # 06
+        print("stop")
+
+
+    if False: # 07
+        print("stop")
+
+
+    if False: # 08
+        print("stop")
+    
