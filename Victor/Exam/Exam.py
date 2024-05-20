0f890042ec859b4b76395545293385a2073c7d7e
VictorLier <victor@lier.dk>
2024-05-20 21:07:02 +0200

    Exam py klar

--- Victor/Exam/Exam.py
+++ Victor/Exam/Exam.py
@@ -1,10 +1,23 @@
+# The functions used here is mostly devolped during the report
+# and is therefore similar to the ones used by Jacob Østergaard (s204408)
+
 import numpy as np
+import math
 import matplotlib.pyplot as plt
+from typing import List
+import scipy.sparse as sps
+import sympy as sp
+import scipy.sparse as sps
+from scipy.linalg import toeplitz
 
-
+def stop():
+    print("stop")
 
 
 if __name__ == '__main__':
+    if True: # Test
+        print("stop")
+
     if True: # 01
         print("stop")
     
